45882266d173bdcca144e8363e27f213fb0606f0
Beaconsyh08 <songyuhao2008@gmail.com>
2020-08-11 11:52:36 +0800

    lab01

---
+++ Lab01/Handout/FirstProgram.java
@@ -0,0 +1,24 @@
+/* 
+   The University of Melbourne
+   School of Computing and Information Systems
+   COMP90041 Programming and Software Development
+   Lecturers: Dr Tilman Dingler and Dr Thuan Pham
+   Semester 2, 2020, Week 2
+   Workshop Sample Program
+   Copyright The University of Melbourne 2020
+   
+   A simple hello world program
+   Jianzhong Qi, 10 February 2016
+*/
+
+public class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Hello reader.");
+		System.out.println("Welcome to Java.");
+
+		System.out.println("Let's demonstrate a simple calculation.");
+		int answer;
+		answer = 2 + 2;
+		System.out.println("2 plus 2 is " + answer);
+	}
+}
