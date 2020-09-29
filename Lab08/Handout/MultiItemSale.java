5af6c6c9c12235f85a4940024c39962eabb3b611
Beaconsyh08 <songyuhao2008@gmail.com>
2020-09-29 14:59:15 +0800

    MultiSale

---
+++ Lab08/Handout/MultiItemSale.java
@@ -0,0 +1,28 @@
+package Solutions.Q7;
+
+/**
+ * Description
+ * This is for Demonstration purpose
+ * If you have any confusion, please contact me
+ * yuhsong1@unimelb.edu.au
+ * yuhsong@student.unimelb.edu.au
+ *
+ * @author Yuhao Song
+ */
+
+public class MultiItemSale {
+    private Sale[] sales;
+    private int curr;
+
+    public MultiItemSale() {
+        // constructor
+    }
+
+    public void insertSale(Sale insert) {
+        // insert sale into the array
+    }
+
+    public double computeTotal() {
+        // compute the total price
+    }
+}
